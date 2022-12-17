e5465329eed380b43c64f4efd215fc53c803e223
Adam Gausmann <adam@gaussian.dev>
2022-12-17 00:33:18 -0600

    Refactor into a reusable Simulation; implement tortoise/hare
    Unfortunately it doesn't finish before running out of memory.
    
    Of course the number of jets in the cycle is a huge prime number,
    which isn't helping.

--- adventofcode/aoc2022/src/bin/day17.rs
+++ adventofcode/aoc2022/src/bin/day17.rs
@@ -28,7 +28,7 @@ fn main() -> anyhow::Result<()> {
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
 
-    let mut jets = input
+    let jets: Vec<i32> = input
         .trim()
         .chars()
         .map(|c| match c {
@@ -36,55 +36,94 @@ fn main() -> anyhow::Result<()> {
             '>' => 1,
             _ => panic!("{}", c),
         })
-        .cycle()
-        .enumerate();
+        .collect();
 
-    let jet_cycle = input.trim().len();
-
-    let mut current_piece = 0;
-    let mut position = IVec2::new(2, 3);
-    let mut stopped = HashSet::new();
-    let mut max_y = 0;
-    let mut count = 0;
-
-    while let Some((ij, dx)) = jets.next() {
-        if PIECES[current_piece]
-            .iter()
-            .map(|&part| position + part + ivec2(dx, 0))
-            .all(|new| new.x < WIDTH && new.x >= 0 && !stopped.contains(&new))
-        {
-            position.x += dx;
-        }
-        //eprintln!("    {:?}", position);
-        if PIECES[current_piece]
-            .iter()
-            .map(|&part| position + part + ivec2(0, -1))
-            .all(|new| new.y >= 0 && !stopped.contains(&new))
-        {
-            position.y -= 1;
-            //eprintln!("    {:?}", position);
-        } else {
-            //eprintln!(": {:?}", position);
-            for &part in PIECES[current_piece] {
-                max_y = max_y.max(position.y + part.y);
-                stopped.insert(position + part);
+    // Part 1
+    {
+        let mut sim = Simulation::new(&jets);
+        loop {
+            sim.step();
+            if sim.piece_count == 2022 {
+                println!("{}", sim.max_y + 1);
+                break;
             }
-            //eprintln!("- {}", max_y);
-
-            current_piece = (current_piece + 1) % PIECES.len();
-            position = IVec2::new(2, max_y + 4);
-            count += 1;
-
-            // Part 1
-            if count == 2022 {
-                println!("{}", max_y + 1);
-            }
-
-            // Part 2
-            // TODO tortoise/hare cycle detection?
         }
-        //eprintln!();
+    }
+
+    // Part 2
+    {
+        let mut tortoise = Simulation::new(&jets);
+        let mut hare = Simulation::new(&jets);
+        loop {
+            tortoise.step();
+            hare.step();
+            hare.step();
+
+            if tortoise.current_position.y == tortoise.max_y + 4
+                && hare.current_position.y == hare.max_y + 4
+                && tortoise.next_jet == hare.next_jet
+                && tortoise.current_piece == hare.current_piece
+            {
+                break;
+            }
+        }
+
+        println!();
+        println!("{} {}", tortoise.max_y + 1, hare.max_y + 1);
     }
 
     Ok(())
+}
+
+struct Simulation<'a> {
+    jets: &'a [i32],
+    next_jet: usize,
+    piece_count: usize,
+    current_piece: usize,
+    current_position: IVec2,
+    stopped: HashSet<IVec2>,
+    max_y: i32,
+}
+
+impl<'a> Simulation<'a> {
+    fn new(jets: &'a [i32]) -> Self {
+        Self {
+            jets,
+            next_jet: 0,
+            piece_count: 0,
+            current_piece: 0,
+            current_position: IVec2::new(2, 3),
+            stopped: HashSet::new(),
+            max_y: 0,
+        }
+    }
+
+    fn step(&mut self) {
+        let dx = self.jets[self.next_jet];
+        self.next_jet = (self.next_jet + 1) % self.jets.len();
+
+        if PIECES[self.current_piece]
+            .iter()
+            .map(|&part| self.current_position + part + ivec2(dx, 0))
+            .all(|new| new.x < WIDTH && new.x >= 0 && !self.stopped.contains(&new))
+        {
+            self.current_position.x += dx;
+        }
+        if PIECES[self.current_piece]
+            .iter()
+            .map(|&part| self.current_position + part + ivec2(0, -1))
+            .all(|new| new.y >= 0 && !self.stopped.contains(&new))
+        {
+            self.current_position.y -= 1;
+        } else {
+            for &part in PIECES[self.current_piece] {
+                self.max_y = self.max_y.max(self.current_position.y + part.y);
+                self.stopped.insert(self.current_position + part);
+            }
+
+            self.current_piece = (self.current_piece + 1) % PIECES.len();
+            self.current_position = IVec2::new(2, self.max_y + 4);
+            self.piece_count += 1;
+        }
+    }
 }
