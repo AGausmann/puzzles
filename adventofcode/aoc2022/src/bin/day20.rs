a2ab78ee1331e38430bf6c8e108ee7ad2e8dfee3
Adam Gausmann <adam@gaussian.dev>
2022-12-23 15:50:42 -0600

    Day 20 Part 2

--- adventofcode/aoc2022/src/bin/day20.rs
+++ adventofcode/aoc2022/src/bin/day20.rs
@@ -8,49 +8,87 @@ fn main() -> anyhow::Result<()> {
     let mut input = String::new();
     stdin().read_to_string(&mut input)?;
 
-    let numbers: Vec<i32> = input
+    let numbers: Vec<i64> = input
         .trim()
         .lines()
         .map(|line| line.parse().unwrap())
         .collect();
-    let n = numbers.len();
-    let mut number_positions: Vec<usize> = (0..n).collect();
 
-    // Indexes to the numbers to track their position, where the indexes are
-    // in the original position of the numbers.
-    let mut indexes: Vec<usize> = (0..n).collect();
+    // Part 1
+    {
+        let mut mixer = Mixer::new(numbers.clone());
+        mixer.mix_once();
+        println!("{}", mixer.coordinates());
+    }
 
-    for i in 0..n {
-        let num = numbers[i];
-        if num < 0 {
-            for _ in 0..num.abs() as usize {
-                let our_pos = number_positions[i];
-                assert_eq!(indexes[our_pos], i);
-                let their_pos = (our_pos + n - 1) % n;
-                assert_eq!(number_positions[indexes[their_pos]], their_pos);
+    // Part 2
+    {
+        let mut mixer = Mixer::new(numbers.iter().map(|x| x * 811589153).collect());
+        for _ in 0..10 {
+            mixer.mix_once();
+        }
+        println!("{}", mixer.coordinates());
+    }
 
-                number_positions.swap(indexes[our_pos], indexes[their_pos]);
-                indexes.swap(our_pos, their_pos);
-            }
-        } else if num > 0 {
-            for _ in 0..num as usize {
-                let our_pos = number_positions[i];
-                assert_eq!(indexes[our_pos], i);
-                let their_pos = (our_pos + 1) % n;
-                assert_eq!(number_positions[indexes[their_pos]], their_pos);
+    Ok(())
+}
 
-                number_positions.swap(indexes[our_pos], indexes[their_pos]);
-                indexes.swap(our_pos, their_pos);
+struct Mixer {
+    n: usize,
+    numbers: Vec<i64>,
+    number_positions: Vec<usize>,
+    indexes_in_position: Vec<usize>,
+}
+
+impl Mixer {
+    fn new(numbers: Vec<i64>) -> Self {
+        let n = numbers.len();
+        Self {
+            n,
+            numbers,
+            number_positions: (0..n).collect(),
+            indexes_in_position: (0..n).collect(),
+        }
+    }
+
+    fn mix_once(&mut self) {
+        for i in 0..self.n {
+            let num = self.numbers[i] % (self.n as i64 - 1);
+            if num < 0 {
+                for _ in 0..num.abs() as usize {
+                    let our_pos = self.number_positions[i];
+                    let their_pos = (our_pos + self.n - 1) % self.n;
+
+                    self.number_positions.swap(
+                        self.indexes_in_position[our_pos],
+                        self.indexes_in_position[their_pos],
+                    );
+                    self.indexes_in_position.swap(our_pos, their_pos);
+                }
+            } else if num > 0 {
+                for _ in 0..num as usize {
+                    let our_pos = self.number_positions[i];
+                    let their_pos = (our_pos + 1) % self.n;
+
+                    self.number_positions.swap(
+                        self.indexes_in_position[our_pos],
+                        self.indexes_in_position[their_pos],
+                    );
+                    self.indexes_in_position.swap(our_pos, their_pos);
+                }
             }
         }
     }
 
-    let zero_point = indexes.iter().take_while(|&&x| numbers[x] != 0).count();
-    let ans: i32 = [1000, 2000, 3000]
-        .into_iter()
-        .map(|offset| numbers[indexes[(zero_point + offset) % n]])
-        .sum();
-    println!("{}", ans);
-
-    Ok(())
+    fn coordinates(&self) -> i64 {
+        let zero_point = self
+            .indexes_in_position
+            .iter()
+            .take_while(|&&x| self.numbers[x] != 0)
+            .count();
+        [1000, 2000, 3000]
+            .into_iter()
+            .map(|offset| self.numbers[self.indexes_in_position[(zero_point + offset) % self.n]])
+            .sum()
+    }
 }
