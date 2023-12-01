1116962ce315ed1e80629fca130e0f423d26a1b4
Adam Gausmann <adam@gaussian.dev>
2023-11-30 22:50:45 -0600

    AOC 2023 template

---
+++ adventofcode/aoc2023/src/bin/template.rs
@@ -0,0 +1,12 @@
+use glam::*;
+use std::cmp::*;
+use std::collections::*;
+use std::io::{stdin, Read};
+use std::ops::*;
+
+fn main() -> anyhow::Result<()> {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input)?;
+
+    Ok(())
+}
